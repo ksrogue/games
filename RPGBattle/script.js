ca2075b520978393a9935a70cdf5b0f2de3b19f4
David William dos Santos <74513553+ksrogue@users.noreply.github.com>
2023-11-20 16:42:19 -0300

    rpg battle

---
+++ RPGBattle/script.js
@@ -0,0 +1,22 @@
+// variaveis que guardam tags html;
+const battleStatus = document.querySelector(".status");
+const monsterHp = document.querySelector(".monster-hp");
+const monsterSprite = document.querySelector(".monster-sprite");
+const playerBtn = document.querySelector(".btn");
+const playerHp = document.querySelector(".player-hp");
+const playerMp = document.querySelector(".player-mp");
+// -------------------------------;
+const player = {
+  name: "ksrogue",
+  atk: 10,
+  hp: 100,
+};
+
+const monster = {
+  name: "lobo",
+  atk: 8,
+  hp: 125,
+};
+
+
+
